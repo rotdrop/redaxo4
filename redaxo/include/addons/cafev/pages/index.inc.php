9cec53a209bad8b909321b1526d1bf83cf274877
Claus-Justus Heine <himself@claus-justus-heine.de>
2016-01-10 13:25:13 +0100

    Make OwnCloud URL configurable and provide some example.

---
+++ redaxo/include/addons/cafev/pages/index.inc.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * CAFeV Addon
+ *
+ * @author Claus-Justus Heine <himself@claus-justus-heine.de>
+ *
+ */
+
+$Basedir = dirname(__FILE__);
+
+$page = rex_request('page', 'string');
+$subpage = rex_request('subpage', 'string');
+$func = rex_request('func', 'string');
+
+require $REX['INCLUDE_PATH'] . '/layout/top.php';
+
+$subpages = array(
+  array('', $I18N->msg('cafev_configuration')),
+  array('example', $I18N->msg('cafev_example')),
+  );
+
+rex_title($I18N->msg('cafev_title'), $subpages);
+
+switch ($subpage) {
+case 'example':
+  require $Basedir . '/example.inc.php';
+  break;
+default:
+  require $Basedir . '/settings.inc.php';
+}
+
+require $REX['INCLUDE_PATH'] . '/layout/bottom.php';
+
+?>
